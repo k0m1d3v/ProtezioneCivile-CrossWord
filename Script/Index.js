23afff70b61f2ad2e944d5ce3124de5e1f748a9a
K0-M1 <86304359+k0m1d3v@users.noreply.github.com>
2023-11-06 19:42:04 +0100

    Game V1.3

--- Script/Index.js
+++ Script/Index.js
@@ -28,21 +28,6 @@ let word10 = false;
 let word11 = false;
 let word12 = false;
 
-let allCells = [
-    "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8","a9", "a10", "a11",
-    "b1", "b2", "b3", "b4", "b5", "b6", "b7",
-    "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8","c9", "c10",
-    "d1", "d2", "d3", "d4", "d5", "d6", "d7",
-    "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9", "e10", "e11",
-    "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8","f9", "f10", "f11",
-    "g1", "g2", "g3", "g4", "g5", "g6",
-    "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8","h9", "h10", "h11", "h12",
-    "i1", "i2", "i3", "i4", "i5", "i6",
-    "j1", "j2", "j3", "j4", "j5", "j6", "j7",
-    "k1", "k2", "k3", "k4", "k5", "k6", "k7", "k8","k9",
-    "l1", "l2", "l3", "l4", "l5", "l6", "l7", "l8"
-];
-
 function showSolutionsFunction(){
     let solStatus = document.getElementById("solutions").checked;
 
